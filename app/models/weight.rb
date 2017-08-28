3d1bd866495b5881ea3e1cb5aff781f903ac5c11
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-28 19:07:43 -0400

    Weight scopes

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -2,4 +2,14 @@ class Weight < ApplicationRecord
   belongs_to :user
 
   default_scope { order(date: :desc) }
+
+  #weights from today, don't use 1.day.ago ase it adds in timestamp
+  scope :today, -> { where('date >= ?', (Date.today -1)) }
+
+  # Most date functions start the week on monday (weird)
+  scope :weekly, -> { where('date >= ?', 1.week.ago) }
+  scope :monthly, -> { where('date >= ?', 1.month.ago) }
+
+  # more control of individual time frame
+  scope :created_after, ->(time) { where('date >= ?', time) if time.present? }
 end
